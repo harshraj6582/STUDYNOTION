d63525c30433ea68b3d0bd2e7584c78ef573b7c6
harshraj6582 <harshraj211430@gmail.com>
2023-11-11 22:23:13 +0530

    3rd Part

--- src/components/core/ExploreMore.jsx
+++ src/components/core/ExploreMore.jsx
@@ -1,87 +1,77 @@
-import React, { useState } from 'react'
-import {HomePageExplore} from "../../data/homepage-explore" 
-import HighlightText from './HomePage/HighlightText';
-
+import React, { useState } from "react";
+import { HomePageExplore } from "../../data/homepage-explore";
+import HighlightText from "./HomePage/HighlightText";
+import CourseCard from "./CourseCard";
 const tabsName = [
-    "Free",
-    "New To Coding ",
-    "Most Poupular " ,
-     "Skill Paths",
-     "Career Paths",
-]
+  "Free",
+  "New To Coding ",
+  "Most Poupular ",
+  "Skill Paths",
+  "Career Paths",
+];
 const ExploreMore = () => {
+  const [currentTab, setCurrentTab] = useState(tabsName[0]);
+  const [courses, setCourses] = useState(HomePageExplore[0].courses);
+  const [currentCard, setCurrentCard] = useState(
+    HomePageExplore[0].courses[0].heading
+  );
 
-
-    const [currentTab , setCurrentTab] = useState(tabsName[0]);
-    const [courses , setCourses] = useState(HomePageExplore[0].courses);
-    const [currentCard  , setCurrentCard] = useState(HomePageExplore[0].courses[0].heading);
-
-
-    const setMyCards = (value) =>{
-        setCurrentTab(value);
-        const result = HomePageExplore.filter((course) => course.tag === value )
-        setCourses(result[0].courses)
-        setCurrentCard(result[0].courses[0].heading)
-    }
+  const setMyCards = (value) => {
+    setCurrentTab(value);
+    const result = HomePageExplore.filter((course) => course.tag === value);
+    setCourses(result[0].courses);
+    setCurrentCard(result[0].courses[0].heading);
+  };
   return (
     <div>
+      <div className="text-4xl font-semibold text-center">
+        Unlock The
+        <HighlightText text={"Power Of Code"} />
+      </div>
 
-
-    <div className='text-4xl font-semibold text-center'>
-        Unlock The 
-        <HighlightText text = {"Power Of Code"}/>
-    </div>
-
-    <p className='text-center text-richblack-300 text-sm mt-3   text-[16px] 
-    px-2 py-2 '>
+      <p
+        className="text-center text-richblack-300 text-sm mt-3   text-[16px] 
+    px-2 py-2 "
+      >
         Learn to Build Anything you Imagine
-    </p>
+      </p>
 
-    <div className='flex flex-row rounded-full bg-richblack-800  mb-5 border-richblack-100 mt-5'>
-        {
-            tabsName.map((element , index) => {
-                return (
-                    <div
-                    className={`text-[16px] flex flex-row items-center gap-2 
-                    ${currentTab === element 
-                    ? "bg-richblack-900  text-richblack-5 "
-                    : "text-richblack-200  "
+      <div className=" flex flex-row rounded-full bg-richblack-800   border-richblack-100 ">
+        {tabsName.map((element, index) => {
+          return (
+            <div
+              className={`text-[20px] flex flex-row w-[70%] items-center gap-2 
+                    ${
+                      currentTab === element
+                        ? "bg-richblack-900  text-richblack-5 "
+                        : "text-richblack-200  "
                     } rounded-full transition-all duration-200 cursor-pointer
                     hover:bg-richblack-900 hover:text-richblack-5 px-5 py-2 `}
-                    key = {index}
-                    onClick = {() => setMyCards(element)}
-                    >
-                        {element}
-                    </div>
-                )
-            })
-        }
+              key={index}
+              onClick={() => setMyCards(element)}
+            >
+              {element}
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="h-[150px]"></div>
+
+      <div className="relative flex flex-wrap gap-10 justify-between w-full mt-5 -ml-5  ">
+        {courses.map((element, index) => {
+          return (
+            <CourseCard
+              key={index}
+              cardData={element}
+              currentCard={currentCard}
+              setCurrentCard={setCurrentCard}
+            />
+          );
+        })}
+      </div>
     </div>
+  );
+};
 
-    <div className='h-[150px]'>
-
-    </div>
-
-
-    <div className='absolute flex flex-row gap-10 justify-between w-full  '>
-        {
-            courses.map((element , index) => {
-                return (
-                    <CourseCard 
-                    key = {index}
-                    cardData = {element}
-                    currentCard = {currentCard}
-                    setCurrentCard = {setCurrentCard}
-                    />
-
-                   
-                )
-            })
-        }
-    </div>
-
-    </div>
-  )
-}
-
-export default ExploreMore
+export default ExploreMore;
